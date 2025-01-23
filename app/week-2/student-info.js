c874bf8b4ab967cc344953199da3acfcdde4298b
feolive <battleshout@hotmail.com>
2025-01-22 23:16:45 -0700

    remove unused files and add effects

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -4,10 +4,15 @@ import GithubIcon from "@/app/components/GithubIcon";
 export default function StudentInfo() {
   return (
     <>
-      <div className="mt-10">
-        <h1 className="text-2xl font-bold">Xiang Wang</h1>
-        <Link href="https://github.com/feolive/cprg306-assignments" alt="github">
-          {/* <Image className="w-12 h-12" src={githubMark} alt="github" /> */}
+      <div className="mt-20 flex flex-col gap-2 items-start justify-center w-50 h-24 p-4 
+      rounded-md shadow-md shadow-slate-500 transition-transform duration-300 ease-in-out hover:-translate-y-2 hover:shadow-lg hover:shadow-blue-700">
+        <h1 className="text-xl font-bold text-blue-700 drop-shadow-md">
+          Xiang Wang
+        </h1>
+        <Link
+          href="https://github.com/feolive/cprg306-assignments"
+          alt="github"
+        >
           <GithubIcon color="var(--foreground)" />
         </Link>
       </div>
