f372d478d81aff7ca84e4aa81553bde456fe6f1c
feolive <battleshout@hotmail.com>
2025-01-30 10:39:48 -0700

    set a fixed width for card

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -2,7 +2,7 @@
 export default function Item({name, quantity, category}) {
     return (
         <>
-            <div className="flex justify-between items-start p-2 rounded-xl shadow-md shadow-black transition-transform duration-600 ease-linear hover:-translate-x-2">
+            <div className="w-56 flex justify-between items-start p-2 rounded-xl shadow-md shadow-black transition-transform duration-600 ease-linear hover:-translate-x-2">
                 <div className="flex flex-col items-start gap-3">
                     <h3 className="font-bold">{name}</h3>
                     <p className="text-sm text-red-700 font-semibold "><span className="outline outline-2 p-1 rounded-xl">{category} </span></p>
