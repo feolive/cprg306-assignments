5a8840a88e50668f35d57f84500c17356873fc42
feolive <battleshout@hotmail.com>
2025-02-04 16:35:48 -0700

    week-4: submission

--- app/page.js
+++ app/page.js
@@ -10,6 +10,7 @@ export default function Home() {
         <div className="mt-10 flex flex-col items-start gap-2 ">
           <Link className="text-blue-500 italic hover:underline underline-offset-1" href="/week-2">Week 2 Assignment</Link>
           <Link className="text-blue-500 italic hover:underline underline-offset-1" href="/week-3">Week 3 Assignment</Link>
+          <Link className="text-blue-500 italic hover:underline underline-offset-1" href="/week-4">Week 4 Assignment</Link>
         </div>
       </div>
     </>
