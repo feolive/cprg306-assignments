5297a2726fd018cad5e597c3cb0689a1d19248a3
feolive <battleshout@hotmail.com>
2025-02-25 22:44:26 -0700

    week-6: optimized the style

--- app/week-6/item-list.js
+++ app/week-6/item-list.js
@@ -6,17 +6,21 @@ import { useState } from "react";
 
 export default function ItemList() {
   const [sortBy, setSortBy] = useState("name");
+  const [prevBtn, setPrevBtn] = useState("name");
 
   const handleSortChange = (e) => {
     setSortBy(e.target.value);
+    document.getElementById(prevBtn).className = "nav-btn";
+    e.target.className = "nav-btn activated";
+    setPrevBtn(e.target.id);
   };
 
   return (
     <div className="flex flex-col items-start gap-2">
       <div className="flex items-center gap-2">
-        <h2 className="text-lg font-bold text-blue-700">Sort By:</h2>
-        <button className="bg-black text-white p-1 rounded-sm" value="name" onClick={(e) => handleSortChange(e)}>Name</button>
-        <button className="bg-red-700 text-white p-1 rounded-sm" value="category" onClick={(e) => handleSortChange(e)}>Category</button>
+        <h2 className="text-lg font-bold text-gray-500 mr-2">Sort By:</h2>
+        <button id="name" className="nav-btn activated" value="name" onClick={(e) => handleSortChange(e)}>Name</button>
+        <button id="category" className="nav-btn" value="category" onClick={(e) => handleSortChange(e)}>Category</button>
       </div>
       {items
         .sort((a, b) =>
--- app/week-6/page.js
+++ app/week-6/page.js
@@ -5,7 +5,7 @@ export default function Page() {
         <>
             <div className="mt-8">
                 <header>
-                    <h1 className="text-2xl font-bold text-blue-700">Shopping List</h1>
+                    <h1 className="text-2xl font-bold text-black italic">Shopping<span className="text-red-700">List</span></h1>
                 </header>
                 <main className="mt-10">
                     <ItemList />
